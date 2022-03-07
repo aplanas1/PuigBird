b8ab93e59b06ad17ac0b334ae92669c0eb502c59
dam2a-2021 <nmoyag01@elpuig.xeill.net>
2022-03-07 19:21:21 +0100

    Initial commit

---
+++ core/src/com/mygdx/bird/Bird.java
@@ -0,0 +1,30 @@
+package com.mygdx.bird;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Bird extends Game {
+
+	SpriteBatch batch;
+	BitmapFont font;
+	int topScore;
+	int lastScore;
+
+	public void create() {
+		batch = new SpriteBatch();
+		// Use LibGDX's default Arial font.
+		font = new BitmapFont();
+		topScore = 0;
+		lastScore = 0;
+
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	public void render() {
+		super.render(); // important!
+	}
+
+	public void dispose() {
+	}
+}
